796e21501b74d1fe873b92329e61ca33dda34f87
mrjosch <mrjosch99@gmail.com>
2025-03-29 00:23:03 +0100

    bump deps

--- lib/src/cli/options.g.dart
+++ lib/src/cli/options.g.dart
@@ -10,38 +10,40 @@ T _$enumValueHelper<T>(Map<T, String> enumValues, String source) =>
     enumValues.entries
         .singleWhere(
           (e) => e.value == source,
-          orElse: () => throw ArgumentError(
-            '`$source` is not one of the supported values: '
-            '${enumValues.values.join(', ')}',
-          ),
+          orElse:
+              () =>
+                  throw ArgumentError(
+                    '`$source` is not one of the supported values: '
+                    '${enumValues.values.join(', ')}',
+                  ),
         )
         .key;
 
 T? _$nullableEnumValueHelperNullable<T>(
   Map<T, String> enumValues,
   String? source,
-) =>
-    source == null ? null : _$enumValueHelper(enumValues, source);
+) => source == null ? null : _$enumValueHelper(enumValues, source);
 
-Options _$parseOptionsResult(ArgResults result) => Options()
-  ..targetOsType = _$nullableEnumValueHelperNullable(
-    _$TargetOsTypeEnumMapBuildCli,
-    result['target-os-type'] as String?,
-  );
+Options _$parseOptionsResult(ArgResults result) =>
+    Options()
+      ..targetOsType = _$nullableEnumValueHelperNullable(
+        _$TargetOsTypeEnumMapBuildCli,
+        result['target-os-type'] as String?,
+      );
 
 const _$TargetOsTypeEnumMapBuildCli = <TargetOsType, String>{
   TargetOsType.linux: 'linux',
   TargetOsType.windows: 'windows',
-  TargetOsType.android: 'android'
+  TargetOsType.android: 'android',
 };
 
-ArgParser _$populateOptionsParser(ArgParser parser) => parser
-  ..addOption(
-    'target-os-type',
-    abbr: 't',
-    help: 'The target OS to install binaries for.',
-    allowed: ['linux', 'windows', 'android'],
-  );
+ArgParser _$populateOptionsParser(ArgParser parser) =>
+    parser..addOption(
+      'target-os-type',
+      abbr: 't',
+      help: 'The target OS to install binaries for.',
+      allowed: ['linux', 'windows', 'android'],
+    );
 
 final _$parserForOptions = _$populateOptionsParser(ArgParser());
 
